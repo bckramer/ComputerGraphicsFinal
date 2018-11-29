18863024d14caec6e39598f1a67e4a56d0c8610f
bckramer <bckramer97@gmail.com>
2018-11-29 16:09:47 -0600

    Moved a lot of logic into callable functions in ParticleObject Changed the particles from spheres to planes. Gives a more "particle"-y look and allows you to spawn way more particles. Spheres crash at 10,000 particles, planes still work (but slow) Added color change. Change the front end to be in rgb rather than hex to make our lives easier.

--- ParticleObject.js
+++ ParticleObject.js
@@ -1,4 +1,6 @@
-var ParticleObject = function(mesh, lifetime, speed, direction)
+const lifeDecay = 0.01;
+
+var ParticleObject = function(mesh, lifetime, speed, direction, startColor, endColor)
 {
     // Sphere mesh
     this.mesh = mesh;
@@ -11,6 +13,45 @@ var ParticleObject = function(mesh, lifetime, speed, direction)
 
     // Direction of object
     this.direction = direction;
+
+    // The color the particle spawns as (THREE.Color)
+    this.startColor = startColor;
+
+    // The color the particle interpolates to throughout its lifetime
+    this.endColor = endColor;
+};
+
+ParticleObject.prototype.updatePosition = function() {
+    this.mesh.position.x = this.mesh.position.x + (this.direction.elements[0] * this.speed);
+    this.mesh.position.y = this.mesh.position.y + (this.direction.elements[1] * this.speed);
+    this.mesh.position.z = this.mesh.position.z + (this.direction.elements[2] * this.speed);
+};
+
+ParticleObject.prototype.updateColor = function() {
+    this.mesh.material.color.r = this.startColor.r + ((1 - this.lifetime) * ((this.endColor.r - this.startColor.r)));
+    this.mesh.material.color.g = this.startColor.g + ((1 - this.lifetime) * (this.endColor.g - this.startColor.g));
+    this.mesh.material.color.b = this.startColor.b + ((1 - this.lifetime) * (this.endColor.b - this.startColor.b));
+};
+
+ParticleObject.prototype.updateLifeTime = function() {
+    this.lifetime = this.lifetime - lifeDecay;
+};
+
+ParticleObject.prototype.resetColor = function() {
+    this.mesh.material.color.r = this.startColor.r;
+    this.mesh.material.color.g = this.startColor.g;
+    this.mesh.material.color.b = this.startColor.b;
+};
+
+ParticleObject.prototype.swapColor = function(newStartColor, newEndColor) {
+    this.startColor = newStartColor;
+    this.endColor = newEndColor;
+};
+
+ParticleObject.prototype.setPosition = function(x, y, z) {
+    this.mesh.position.x = x;
+    this.mesh.position.y = y;
+    this.mesh.position.z = z;
 };
 
 ParticleObject.prototype.getMesh = function()
--- ParticleSystem.js
+++ ParticleSystem.js
@@ -8,14 +8,63 @@ const ASPECT = WIDTH / HEIGHT;
 const NEAR = 0.1;
 const FAR = 10000;
 const numParticles = 1000;
+var startColor;
+var endColor;
 var objects = [];
 
 var compactability = document.getElementById("compactability").value;
 var lifetime = document.getElementById("lifetime").value;
 var size = document.getElementById("size").value;
 var speed = document.getElementById("speed").value;
-var startColor = document.getElementById("startColor").value;
-var endColor = document.getElementById("endColor").value;
+var startColorR = document.getElementById("startColorR").value;
+var startColorG = document.getElementById("startColorG").value;
+var startColorB = document.getElementById("startColorB").value;
+var endColorR = document.getElementById("endColorR").value;
+var endColorG = document.getElementById("endColorG").value;
+var endColorB = document.getElementById("endColorB").value;
+function updateTextBoxes() {
+    this.compactability = document.getElementById("compactability").value;
+    this.lifetime = document.getElementById("lifetime").value;
+    this.size = document.getElementById("size").value;
+    this.speed = document.getElementById("speed").value;
+    startColor.r = this.startColorR = document.getElementById("startColorR").value;
+    startColor.g = this.startColorG = document.getElementById("startColorG").value;
+    startColor.b = this.startColorB = document.getElementById("startColorB").value;
+    endColor.r = this.endColorR = document.getElementById("endColorR").value;
+    endColor.g = this.endColorG = document.getElementById("endColorG").value;
+    endColor.b = this.endColorB = document.getElementById("endColorB").value;
+}
+
+var xSpeed = 3.0;
+var ySpeed = 3.0;
+var zSpeed = 3.0;
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 87) {
+        //w
+        user.position.z -= zSpeed;
+    } else if (keyCode == 83) {
+        //s
+        user.position.z += zSpeed;
+    } else if (keyCode == 65) {
+        //a
+        user.position.x -= xSpeed;
+    } else if (keyCode == 68) {
+        //d
+        user.position.x += xSpeed;
+    } else if (keyCode == 82) {
+        //r
+        user.position.y += ySpeed;
+    } else if (keyCode == 70) {
+        //f
+        user.position.y -= ySpeed;
+    } else if (keyCode == 32) {
+        //space
+        user.position.set(0, 0, 0);
+    }
+};
 
 // Get the DOM element to attach to
 const container =
@@ -79,66 +128,6 @@ const user = new THREE.Mesh(
 user.position.z = -300;
 scene.add(user);
 
-
-var xSpeed = 3.0;
-var ySpeed = 3.0;
-var zSpeed = 3.0;
-
-document.addEventListener("keydown", onDocumentKeyDown, false);
-function onDocumentKeyDown(event) {
-    var keyCode = event.which;
-    if (keyCode == 87) {
-        //w
-        user.position.z -= zSpeed;
-    } else if (keyCode == 83) {
-        //s
-        user.position.z += zSpeed;
-    } else if (keyCode == 65) {
-        //a
-        user.position.x -= xSpeed;
-    } else if (keyCode == 68) {
-        //d
-        user.position.x += xSpeed;
-    } else if (keyCode == 82) {
-        //r
-        user.position.y += ySpeed;
-    } else if (keyCode == 70) {
-        //f
-        user.position.y -= ySpeed;
-    } else if (keyCode == 32) {
-        //space
-        user.position.set(0, 0, 0);
-    }
-};
-
-//    var particleCount = 1800,
-//        particles = new THREE.Geometry(),
-//        pMaterial = new THREE.PointsMaterial({
-//            color: 0xFF0000,
-//            size: 10000
-//        });
-
-//    for (var p = 0; p < particleCount; p++) {
-//
-//        // create a particle with random
-//        // position values, -250 -> 250
-//        var pX = Math.random() * 500 - 250,
-//            pY = Math.random() * 500 - 250,
-//            pZ = Math.random() * 500 - 250,
-//            particle = new THREE.Vector3(
-//                new THREE.Vector3(pX, pY, pZ)
-//            );
-//
-//        // add it to the geometry
-//        particles.vertices.push(particle);
-//    }
-//
-//    var particleSystem = new THREE.Points(
-//        particles,
-//        pMaterial);
-//
-//    scene.add(particleSystem);
-
 for (var i = 0; i < numParticles; i++) {
     let particleMaterial =
         new THREE.MeshPhongMaterial(
@@ -146,47 +135,54 @@ for (var i = 0; i < numParticles; i++) {
                 color: new THREE.Color(0xFFFFFF)
             });
     let particleMesh = new THREE.Mesh(
-        new THREE.SphereGeometry(
+        // new THREE.SphereGeometry(
+        //     0.5,
+        //     RADIUS,
+        //     RINGS),
+        new THREE.PlaneGeometry(
             1.0,
-            SEGMENTS,
-            RINGS),
+            1.0,
+            1.0),
 
         particleMaterial);
-    particleMesh.position.x = user.position.x;
-    particleMesh.position.y = user.position.y;
-    particleMesh.position.z = user.position.z;
+    particleMesh.position.x = user.position.x + Math.random() * 20 - 10;
+    particleMesh.position.y = user.position.y + Math.random() * 20 - 10;
+    particleMesh.position.z = user.position.z + Math.random() * 20 - 10;
+    this.startColor = new THREE.Color(this.startColorR, this.startColorG, this.startColorB);
+    this.endColor = new THREE.Color(this.endColorR, this.endColorG, this.endColorB);
 
-    let particle = new ParticleObject(particleMesh, Math.random(), Math.random() * .5, new Vector3([1 - (2 * Math.random()), 1 - (2 *Math.random()), 1 - (2 * Math.random())]));
+    let particle =
+        new ParticleObject(
+                particleMesh, //Mesh
+                this.lifetime * Math.random(), //Lifetime. If there is no random, all the particle will spawn and die at the same time
+                this.speed, //Speed
+                new Vector3([1 - (2 * Math.random()), 1 - (2 *Math.random()), 1 - (2 * Math.random())]), //Direction
+                new THREE.Color(this.startColor), //StartColor
+                new THREE.Color(this.endColor) //EndColor
+            );
+
 
     objects.push(particle);
     scene.add(particle.mesh);
-
-    let newParticle = new ParticleObject(
-        particleMesh,
-        1.0,
-        0.0,
-        new Vector3([0, 0, 0])
-    );
 }
 
 function update() {
+    updateTextBoxes();
     renderer.render(scene, camera);
     for (var i = 0; i < objects.length; i++) {
         //    objects[i].position.x = Math.random() * 250 - 175;
         if (objects[i].lifetime < 0) {
-            objects[i].mesh.position.x = user.position.x + Math.random() * 20 - 10;
-            objects[i].mesh.position.y = user.position.y;
-            objects[i].mesh.position.z = user.position.z + Math.random() * 20 - 10;
-            objects[i].lifetime = Math.random();
-            objects[i].mesh.material.color = new THREE.Color(0xFFFFFF);
+            let spawnX = user.position.x + Math.random() * 20 - 10;
+            let spawnY = user.position.y;
+            let spawnZ = user.position.z + Math.random() * 20 - 10;
+            objects[i].setPosition(spawnX, spawnY, spawnZ);
+            objects[i].lifetime = this.lifetime;
+            objects[i].resetColor();
         } else {
-            objects[i].mesh.position.x =  objects[i].mesh.position.x + (objects[i].direction.elements[0] * objects[i].speed);
-            objects[i].mesh.position.y =  objects[i].mesh.position.y + (objects[i].direction.elements[1] * objects[i].speed);
-            objects[i].mesh.position.z =  objects[i].mesh.position.z + (objects[i].direction.elements[2] * objects[i].speed);
-            objects[i].mesh.material.color.r = objects[i].mesh.material.color.r - 0.005;
-            // objects[i].mesh.material.color.r = objects[i].mesh.material.color.g - 0.005;
-            // objects[i].mesh.material.color.r = objects[i].mesh.material.color.b - 0.005;
-            objects[i].lifetime = objects[i].lifetime - 0.01;
+            objects[i].updatePosition();
+            objects[i].updateLifeTime();
+            objects[i].updateColor();
+            objects[i].swapColor(this.startColor, this.endColor)
         }
     }
     requestAnimationFrame(update);
